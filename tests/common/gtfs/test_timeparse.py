f4bb9b1cde860a7428f63e8118bf78bd386ec105
grzechuzz <grzechu.rz@gmail.com>
2025-12-23 17:12:04 +0000

    Add timeparse test

---
+++ tests/common/gtfs/test_timeparse.py
@@ -0,0 +1,20 @@
+import pytest
+from app.common.app_common.gtfs.timeparse import parse_gtfs_time_to_seconds
+
+
+def test_basic_times():
+    assert parse_gtfs_time_to_seconds("00:00:00") == 0
+    assert parse_gtfs_time_to_seconds("01:02:03") == 3723
+
+
+def test_above_24():
+    assert parse_gtfs_time_to_seconds("25:10:00") == 25 * 3600 + 10 * 60
+
+
+@pytest.mark.parametrize(
+    "value",
+    ["aa", "10:70:00", "10:00:70", "", None]
+)
+def test_invalid_times(value):
+    with pytest.raises(ValueError):
+        parse_gtfs_time_to_seconds(value)
