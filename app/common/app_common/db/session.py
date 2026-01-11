713e02709b5b40252b12debf4e9ea9cb50fa1e68
grzechuzz <grzechu.rz@gmail.com>
2026-01-11 19:52:23 +0100

    refactor: delete files, change project structure

--- app/common/app_common/db/session.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from app.common.app_common.config import build_database_url
-
-engine = create_engine(build_database_url(), pool_pre_ping=True)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
