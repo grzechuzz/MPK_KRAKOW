daa795ade459d72bb0212de582e41f3b74b0bf0c
grzechuzz <grzechu.rz@gmail.com>
2026-02-10 17:36:41 +0100

    feat: add annotated DbSession

--- app/api/db.py
+++ app/api/db.py
@@ -1,5 +1,7 @@
 from collections.abc import Generator
+from typing import Annotated
 
+from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from app.common.db.connection import get_session
@@ -8,3 +10,6 @@ from app.common.db.connection import get_session
 def get_db() -> Generator[Session]:
     with get_session() as session:
         yield session
+
+
+DbSession = Annotated[Session, Depends(get_db)]
